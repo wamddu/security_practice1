ef9d84c064773fb7a60f764bfb2178b038a1d1ed
wamddu <yhy6923@naver.com>
2026-02-04 14:30:44 +0900

    Repository 구현

---
+++ src/main/java/com/asdf/practice1/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.asdf.practice1.repository;
+
+import com.asdf.practice1.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
---
+++ src/main/java/com/asdf/practice1/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.asdf.practice1.repository;
+
+import com.asdf.practice1.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    Optional<User> findUserByUsername(String username);
+}
