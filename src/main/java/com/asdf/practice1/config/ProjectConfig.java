838b55406758ad81314dc9fdf4a1c7bc4c3c6e15
wamddu <yhy6923@naver.com>
2026-02-04 14:30:31 +0900

    Config 구현

--- src/main/java/com/asdf/practice1/config/ProjectConfig.java
+++ src/main/java/com/asdf/practice1/config/ProjectConfig.java
@@ -1,15 +1,37 @@
 package com.asdf.practice1.config;
 
+import com.asdf.practice1.security.AuthenticationProviderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class ProjectConfig {
+@EnableWebSecurity
+public class ProjectConfig{
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/main", true)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 }
